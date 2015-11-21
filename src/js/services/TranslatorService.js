cd03e9f178f44e9466c1e32d1896756f7056623a
Scott Pullen <scottpullen@g.harvard.edu>
2015-11-21 10:25:04 -0500

    Adding test for the zombie translator

--- src/js/services/TranslatorService.js
+++ src/js/services/TranslatorService.js
@@ -2,18 +2,16 @@ angular.module('DRRrrRrvrr')
 .service('TranslatorService', ['$http', function($http) {
   
   this.translate = function(text, callback) {
-    if(text !== undefined) {
-      var translatedText = '';
-      $http.get('http://ancient-anchorage-9224.herokuapp.com/zombify', { params: 
-        {
-          q: text
-        }
-      }).then(function(response){
-        translatedText = response.data.message;
-        if(callback) {
-          callback(translatedText);
-        }
-      });
-    }
+    var translatedText = '';
+    $http.get('http://ancient-anchorage-9224.herokuapp.com/zombify', { 
+      params: {
+        q: text
+      }
+    }).then(function(response){
+      translatedText = response.data.message;
+      if(callback) {
+        callback(translatedText);
+      }
+    });
   };
 }]);
--- src/tests/TranslatorServiceSpec.js
+++ src/tests/TranslatorServiceSpec.js
@@ -2,17 +2,27 @@ describe('TranslatorService', function() {
   var translator,
       $httpBackend;
 
+  var zombifyMock = { message: 'BRAINS' };    
+
   beforeEach(function() {
     module('DRRrrRrvrr');
 
     inject(function($injector){
-      translator = $injector.get('posts');
+      translator = $injector.get('TranslatorService');
       $httpBackend = $injector.get('$httpBackend');
     });
 
     $httpBackend
-      .when('GET', 'http://ancient-anchorage-9224.herokuapp.com/zombify?q=')
-      .respond(200, 'BRAINS');
-      
+      .when('GET', 'http://ancient-anchorage-9224.herokuapp.com/zombify')
+      .respond(200, zombifyMock);
+  });
+
+  describe('#translate', function() {
+    it('should translate the text to zombie', function() {
+      var text = 'SOMETHING';
+      translator.translate(text, function(translatedText) {
+        expect(translatedText).toBe(zombifyMock.message);
+      });
+    });
   });
 });
