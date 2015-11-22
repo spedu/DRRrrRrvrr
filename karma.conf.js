54075c5ddf964a0c343bd3b2fff16353aeb874e7
Scott Pullen <scottpullen@g.harvard.edu>
2015-11-22 13:12:28 -0500

    Fixing issue with ng-html2js-preprocessor

--- karma.conf.js
+++ karma.conf.js
@@ -7,11 +7,15 @@ module.exports = function(config) {
     // base path that will be used to resolve all patterns (eg. files, exclude)
     basePath: '',
 
+    plugins: [
+      require('./node_modules/karma-jasmine'),
+      require('./node_modules/karma-ng-html2js-preprocessor'),
+      require('./node_modules/karma-phantomjs-launcher')
+    ],
 
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
     frameworks: ['jasmine'],
-
 
     // list of files / patterns to load in the browser
     files: [ 
